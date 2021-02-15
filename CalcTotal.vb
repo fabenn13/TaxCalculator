ad56829dfdba6adb934b490d50e078566caf299d
fabenn13 <57354700+fabenn13@users.noreply.github.com>
2021-02-15 17:09:04 -0600

    Add files via upload
    Code for a modular version.

---
+++ CalcTotal.vb
@@ -0,0 +1,11 @@
+﻿Public Class CalcTotal
+    Private Tax As Double
+    Private Value As Double
+    Private Disc As Double
+
+    Public Function CalculateTotal(Tax, Value, Disc) As Double
+        Dim SubTotal As Double = Value - ((Disc / 100) * Value)
+        Dim Total As Double = SubTotal + ((Tax / 100) * SubTotal)
+        Return Total
+    End Function
+End Class
---
+++ Form1.vb
@@ -0,0 +1,14 @@
+﻿Public Class Form1
+    Dim taxRate As Double
+    Dim val1 As Double
+    Dim discount As Double
+
+    Private Sub btnCalc_Click(sender As System.Object, e As System.EventArgs) Handles btnCalc.Click
+        taxRate = txtTax.Text
+        val1 = txtPrice.Text
+        discount = txtDisc.Text
+        Dim ct As New CalcTotal
+
+        lblTotal.Text = ct.CalculateTotal(taxRate, val1, discount).ToString("c")
+    End Sub
+End Class
